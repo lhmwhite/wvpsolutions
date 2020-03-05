6cf9bb3c91c754abe21eb39e537bd8299ecadb27
Colin Birdwell <Colin.birdwell@paymentdata.com>
2020-03-04 22:58:49 -0600

    removed unnessary files and fixed build error

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
 import DashboardPage from '../components/DashboardPage'
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
-import AddExpensePage from '../components/AddExpensePage'
-import EditExpensePage from '../components/EditExpensePage'
+
 import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFoundPage'
 import Header from '../components/Header'
@@ -14,9 +12,6 @@ const AppRouter = () => (
       <Header />
       <Switch>
         <Route path="/" component={DashboardPage} exact={true} />
-        <Route path="/expensedash" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
