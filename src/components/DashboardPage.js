ebcce01a8bbb5961fb55d637bab6f0a90e092c20
Colin Birdwell <Colin.birdwell@paymentdata.com>
2020-04-09 11:53:21 -0500

    added text for body on dashboard for development purposes

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -11,27 +11,17 @@ const DashboardPage = () => (
       <div className="col-8">
         <Fade duration={1000}>
           <div className="pallet pallet-2">
-            <h2>Subtitle Bree Serif</h2>
-            <p>This is Raleway</p>
+            <h2>One size does not fit all.</h2>
             <p>
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-              egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
-              Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
-              placerat eleifend leo. <span>Quisque sit amet</span> est et sapien ullamcorper
-              pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
-              Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis
-              tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.
-              Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate
-              magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan
-              porttitor, facilisis luctus, metus
+              WVP Solutions focuses on small business and individual benefits including disability,
+              health insurance, life insurance and supplemental coverage.
+            </p>
+            <p>
+              Find the solutions that work for your situation, whether it includes ACA individual
+              options, group benefits, healthshares or Medicare.
             </p>
 
-            <p>
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-              egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
-              Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
-              placerat eleifend leo.
-            </p>
+            <p>Contact us to discuss how we can help you.</p>
           </div>
         </Fade>
       </div>
