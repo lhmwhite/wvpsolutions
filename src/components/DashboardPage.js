bee527e37992eaf47d9bfcda59d76c4a73e59234
Colin Birdwell <Colin.birdwell@paymentdata.com>
2020-05-01 11:17:45 -0500

    Styled CTA's

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
 import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import {
+  faPrescriptionBottleAlt,
+  faBuilding,
+  faHardHat,
+  faBaby,
+  faHeart,
+} from '@fortawesome/free-solid-svg-icons'
 import ChangeLanguage from './ChangeLanguage'
 import { _onLanguageChange } from '../actions'
 import { testESP, testEng } from '../data'
+
+const navItem = (to, icon, text) => (
+  <NavLink to={to} activeClassName={false} className="nav-item">
+    <FontAwesomeIcon icon={icon} className="nav-icon" />
+    <p>{text}</p>
+  </NavLink>
+)
 
 const DashboardPage = props => {
   const { language } = props
@@ -28,6 +44,16 @@ const DashboardPage = props => {
               </p>
 
               <p>Contact us to discuss how we can help you.</p>
+
+              <div className="row" style={{ margin: '0 auto 20px auto', width: '75%' }}>
+                {navItem('/', faPrescriptionBottleAlt, 'Medicare')}
+                {navItem('/create', faBuilding, 'Business')}
+                {navItem('/create', faHardHat, 'Supplemental Benefits')}
+              </div>
+              <div className="row">
+                {navItem('/create', faBaby, 'Individual & Family')}
+                {navItem('/create', faHeart, 'Family Protection & Retirement')}
+              </div>
             </div>
           </Fade>
         </div>
